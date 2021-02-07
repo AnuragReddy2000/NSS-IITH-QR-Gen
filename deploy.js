da09e31cf26912627c7302e4e18f4dc6bbb5718b
anuragreddy2000 <anuragreddy1000@gmail.com>
2021-02-07 23:25:16 +0530

    Modified deploy script

--- deploy.js
+++ deploy.js
@@ -4,8 +4,9 @@ const exec = util.promisify(require('child_process').exec);
 let build = async () => {
     console.log("delploying to gh-pages");
     await exec("git checkout gh-pages");
+    await exec("git pull");
     console.log("removing existing files from gh-pages");
-    await exec("git rm --f -r *");
+    await exec("git rm --cached -r *");
     await exec('git commit -m "deleting previous build"');
     console.log("adding new build to gh-pages");
     await exec("git add -f build/*");
