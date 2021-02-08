f06f0e49f2cf31963b36433f272b8223788e193c
anuragreddy2000 <anuragreddy1000@gmail.com>
2021-02-08 08:35:29 +0530

    deploy script done

--- deploy.js
+++ deploy.js
@@ -7,9 +7,10 @@ let build = async () => {
     await exec("git pull");
     console.log("removing existing files from gh-pages");
     await exec("git rm --cached -r *");
-    await exec("git checkout .");
-    await exec("cp /build/* .");
+    await exec("git add .gitignore");
+    await exec("git clean -f -d");
     console.log("adding new build to gh-pages");
+    await exec("xcopy /s build .");
     await exec("git add .");
     await exec('git commit -m "build successful"');
     console.log("pushing changes");
